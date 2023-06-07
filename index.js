41d2ca8926c71b56c58fce9480f5ccf6a31ba36d
sahnoune2 <anankarta@gmail.com>
2023-06-07 21:51:31 +0100

    index.js

--- index.js
+++ index.js
@@ -30,28 +30,45 @@ function node(data) {
   };
 }
 
-function tree(array, start, end) {
-  if (start > end) {
-    return null;
-  }
+function tree() {
+  return {
+    buildTree: function (array, start, end) {
+      if (start > end) {
+        return null;
+      }
 
-  let mid = parseInt((start + end) / 2);
+      let mid = parseInt((start + end) / 2);
 
-  let head = new node(array[mid]);
+      let head = new node(array[mid]);
 
-  head.left = tree(array, start, mid - 1);
-  head.right = tree(array, mid + 1, end);
+      head.left = this.buildTree(array, start, mid - 1);
+      head.right = this.buildTree(array, mid + 1, end);
 
-  return head;
+      return head;
+    },
+
+    search: function (root, key) {
+      if (root === null || root.data === key) {
+        return root;
+      }
+      if (key > root.data) {
+        return this.search(root.right, key);
+      } else if (key < root.data) {
+        return this.search(root.left, key);
+      }
+    },
+  };
 }
 
 let seif = [0, 2, 1, 0, 5, 0, 9, 5, 0, 2, 1, 0, 3];
 
 let arrayTree = sort(duplicate(seif));
+let total = tree();
+
 console.log(arrayTree);
 let n = arrayTree.length;
 console.log(n);
-let root = tree(arrayTree, 0, n - 1);
+let root = total.buildTree(arrayTree, 0, n - 1);
 
 function print(node) {
   let result = "";
@@ -65,3 +82,7 @@ function print(node) {
 }
 
 console.log(print(root));
+
+let find = total.search(root, 9);
+
+console.log(find);
